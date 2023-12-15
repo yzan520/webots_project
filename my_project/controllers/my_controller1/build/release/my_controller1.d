43f160ae06290596bdfd668c481c7c0c92ef174d
yzan520 <1006659217@qq.com>
2023-12-15 10:04:31 +0800

    the first commit

---
+++ my_project/controllers/my_controller1/build/release/my_controller1.d
@@ -0,0 +1,5 @@
+build/release/my_controller1.o: my_controller1.c \
+ /usr/local/webots/include/controller/c/webots/motor.h \
+ /usr/local/webots/include/controller/c/webots/types.h \
+ /usr/local/webots/include/controller/c/webots/robot.h \
+ /usr/local/webots/include/controller/c/webots/nodes.h
